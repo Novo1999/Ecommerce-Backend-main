d78ffa9f008c316810605d1df3fce8cf00c47f0c
Novo1999 <neptune.navo@gmail.com>
2023-10-29 11:03:20 +0600

    test

--- src/middleware/errorHandlerMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var http_status_codes_1 = require("http-status-codes");
-var errorHandlerMiddleware = function (err, req, res, next) {
-    var statusCode = err.statusCode || http_status_codes_1.StatusCodes.INTERNAL_SERVER_ERROR;
-    var msg = err.message || 'Something went wrong';
-    res.status(statusCode).json({ msg: msg });
-};
-exports.default = errorHandlerMiddleware;
